e79b60333d24850fdb5e0bbbffc97047cf2ec6b1
sasank <121434861+sasank-pankam@users.noreply.github.com>
2024-07-16 21:47:10 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,12 +1,12 @@
 // export const frontendIp = "192.168.1.4";
 // export const frontendIp = "localhost";
-export const frontendPort = "8080";
+// export const frontendPort = "8080";
 
 export const frontendDomain = process.env.FRONTEND_DOMAIN;
 
 export const frontendUrls = {
-  home: `http://${frontendDomain}:${frontendPort}/`,
-  loginError: `http://${frontendDomain}:${frontendPort}/error`,
+  home: `http://${frontendDomain}/`,
+  loginError: `http://${frontendDomain}/error`,
 };
 
 export const serverDomain = "sodi.ddnsgeek.com";
