c373f6b40453db14d20097e3e8e7ec6bce23bbd2
sasank pankam <sasaankme@gmail.com>
2024-07-16 15:01:36 +0530

    exposed to network

--- constants.js
+++ constants.js
@@ -1,13 +1,20 @@
-// export const frontendIp = "172.16.207.102";
-export const frontendIp = "localhost";
-export const frontendPort = "5173";
+// export const frontendIp = "192.168.1.4";
+// export const frontendIp = "localhost";
+export const frontendPort = "8080";
 
+export const frontendDomain = process.env.FRONTEND_DOMAIN;
 
 export const frontendUrls = {
-    home: `http://${frontendIp}:${frontendPort}/`,
-    loginError: `http://${frontendIp}:${frontendPort}/error`,
-}
+  home: `http://${frontendDomain}:${frontendPort}/`,
+  loginError: `http://${frontendDomain}:${frontendPort}/error`,
+};
 
+export const serverDomain = "sodi.ddnsgeek.com";
+
+// export const frontendUrls = {
+//   home: `/`,
+//   loginError: `/error`,
+// };
 export const pageSize = 10;
 
-export const serverIp = "172.16.207.102";
+export const serverIp = "0.0.0.0";
--- index.js
+++ index.js
@@ -2,95 +2,138 @@ import express from "express";
 import bodyParser from "body-parser";
 // import { config } from "dotenv";
 import morgan from "morgan";
-import authroute from "./routers/authRoute.js"
+import authroute from "./routers/authRoute.js";
 import { connectDb } from "./config/DbConnect.js";
 import { errorHandler, notFound } from "./middlewares/errorHandler.js";
 import session from "express-session";
 import passport from "passport";
 import { CatchError } from "./middlewares/CatchError.js";
-import cookieParser from 'cookie-parser';
-import complaintRoute from "./routers/complaintRoute.js"
-import fileupload from "express-fileupload"
+import cookieParser from "cookie-parser";
+import complaintRoute from "./routers/complaintRoute.js";
+import fileupload from "express-fileupload";
+import cors from "cors";
+import MongoDBStoreFactory from "connect-mongodb-session";
+import { frontendDomain, frontendUrls, serverIp } from "./constants.js";
+import { User, UserTypes } from "./models/UserModel.js";
+// import path from "path";
+import http from "http";
+import { log } from "console";
 
-import MongoDBStoreFactory from 'connect-mongodb-session';
-import {frontendUrls, serverIp} from "./constants.js";
-import {User, UserTypes} from "./models/UserModel.js";
 const MongoDBStore = MongoDBStoreFactory(session);
 
 // config({
 //     path:'./.env'
 // })
 
-const store = MongoDBStore({
-    uri:process.env.MONGO_URL,
-    collection: 'sessions',
-})
-
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URL,
+  collection: "sessions",
+});
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(fileupload({
-    useTempFiles: true,
-    tempFileDir: '/tmp/'
-  }));
-app.use(morgan("dev"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.set('trust proxy', 1);
+app.use(
+  cors({
+    origin: frontendDomain, // Allow requests from this origin
+    credentials: true, // Allow cookies to be sent with the request
+  }),
+);
+
+// app.set('trust proxy', 1);
 app.use(cookieParser());
-app.use(session({
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET_KEY,
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//       domain: frontendDomain,
+//       path: "/",
+//       secure: true,
+//       maxAge: 24 * 60 * 60 * 1000, // 24 hrs
+//       sameSite: "none",
+//     },
+//     store,
+//   }),
+// );
+
+app.use(
+  session({
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: false,
-        maxAge: 24 * 60 * 60 * 1000, // 24 hrs
+      domain: `${frontendDomain}`,
+      // path: "/",
+      // secure: true,
+      maxAge: 24 * 60 * 60 * 1000, // 24 hrs
+      sameSite: "none",
     },
     store,
-}));
+  }),
+);
+
+app.use(morgan("dev"));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/grievance/auth',authroute)
-app.use('/grievance',complaintRoute)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  }),
+);
 
-app.get("/",(req,res)=>{
-    res.redirect(frontendUrls.home);
-    // res.send('Hello World');
+app.use("/grievance/auth", authroute);
+app.use("/grievance", complaintRoute);
+
+app.get("/", (req, res) => {
+  res.redirect(frontendUrls.home);
+  // res.sendFile("/dist/index.html", { root: process.cwd() });
+  // res.send('Hello World');
 });
 
-app.all("*",(req,res,next)=>{
-    // const err = new Error(`Route ${req.originalUrl} not Found`);
-    res.redirect(frontendUrls.loginError);
-})
+// app.all("*",(req,res,next)=>{
+//     // const err = new Error(`Route ${req.originalUrl} not Found`);
+//     res.redirect(frontendUrls.loginError);
+// })
 
-// app.use(CatchError)
+app.use(CatchError);
 app.use(notFound);
-app.use(errorHandler)
+app.use(errorHandler);
 
-connectDb().then( async ()=>{
-    // app.listen(PORT, '0.0.0.0' ,() => {
+const server = http.createServer(app);
+
+connectDb()
+  .then(async () => {
+    // app.listen(PORT, serverIp ,() => {
     //     console.log(`Server started on http://${serverIp}:${PORT}`);
     // } )
-    app.listen(PORT, serverIp, () => {
-        console.log(`Server started on http://${serverIp}:${PORT}`);
-});
+    //     app.listen(PORT, serverIp, () => {
+    //         console.log(`Server started on http://${serverIp}:${PORT}`);
+    // });
+    server.listen(PORT, serverIp, () => {
+      console.log(`Server started on http://${serverIp}:${PORT}`);
+    });
     const adminEmail = process.env.ADMIN_EMAIL;
-    // console.log('Admin Email:', adminEmail);
-    const admin = await User.findOne({email:adminEmail});
-    // console.log('Admin', admin);
-    if(!admin){
-        // console.log('Admin not found, creating one');
-        const result = await User.create({
-            email:adminEmail,
-            userType:UserTypes.Admin,
-        })
-        if(!result) throw new Error('Admin not created');
-        process.exit(1);
+    // console.log("Admin Email:", adminEmail);
+    const admin = await User.findOne({ email: adminEmail });
+    // console.log("Admin", admin);
+    if (!admin) {
+      // console.log('Admin not found, creating one');
+      const result = await User.create({
+        email: adminEmail,
+        userType: UserTypes.Admin,
+      });
+      if (!result) throw new Error("Admin not created");
+      else console.log("admin added sucessfully");
+      process.exit(1);
     }
-}).catch((err)=>{
-    console.error('-------> Connection error <-------', err);
+  })
+  .catch((err) => {
+    console.error("-------> Connection error <-------", err);
     process.exit(1);
-})
-
+  });
